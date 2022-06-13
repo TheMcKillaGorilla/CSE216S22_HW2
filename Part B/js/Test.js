ccbd4baf088721fc50ae77a46219981ad92b91ce
CSStudentSBU <82350450+CSStudentSBU@users.noreply.github.com>
2022-06-13 00:32:10 -0400

    Update Test.js

--- Part B/js/Test.js
+++ Part B/js/Test.js
@@ -21,24 +21,24 @@ function addPersonToBST(person, tree) {
 let tree = new BinarySearchTree(KEY_LENGTH);
 
 // DEMONSTRATE ADDING VALUES TO THE BST, WHICH INCLUDES THE NEED TO MAKE THE BST BIGGER
-addPersonToBST(new Student(tree.generateKey(), "George", "Harrison", 4.0), tree);
-addPersonToBST(new Employee(tree.generateKey(), "Paul", "McCartney", 80000), tree);
-addPersonToBST(new Employee(tree.generateKey(), "Ringo", "Starr", 40000), tree);
-addPersonToBST(new Person(tree.generateKey(), "Chuck", "Berry"), tree);
-addPersonToBST(new Student(tree.generateKey(), "Mick", "Jagger", 3.5), tree);
-addPersonToBST(new Student(tree.generateKey(), "Jimi", "Hendrix", 3.6), tree);
-addPersonToBST(new Person(tree.generateKey(), "Roger", "Waters"), tree);
+addPersonToBST(new Student(tree.generateKey(), "Harvard", "Crimson", 4.0), tree);
+addPersonToBST(new Employee(tree.generateKey(), "Yale", "Law", 80000), tree);
+addPersonToBST(new Employee(tree.generateKey(), "Jake", "Peralta", 40000), tree);
+addPersonToBST(new Person(tree.generateKey(), "Amy", "Santiago"), tree);
+addPersonToBST(new Student(tree.generateKey(), "Micheal", "Scott", 3.5), tree);
+addPersonToBST(new Student(tree.generateKey(), "Steve", "Harvey", 3.6), tree);
+addPersonToBST(new Person(tree.generateKey(), "Rock", "Dwayne"), tree);
 
 // DEMONSTRATE MAKING KEYS AND ADDING VALUES TO THE BST    
 let jlKey = tree.generateKey();
-tree.putValue(jlKey, new Student(jlKey, "John", "Lennon", 3.8));
+tree.putValue(jlKey, new Student(jlKey, "Tony", "Stark", 3.8));
 let cwKey = tree.generateKey();
-tree.putValue(cwKey, new Student(cwKey, "Charlie", "Watts", 3.1));
+tree.putValue(cwKey, new Student(cwKey, "Steve", "Rogers", 9.1));
 let dgKey = tree.generateKey();
 tree.putValue(dgKey, new Employee(dgKey, "David", "Gilmour", 120000));
 printBST("\nAfter Changing 3 Items", tree);
 
-// DEMONSTRATE GETTING VALUES FROM THE BST
+// DEMONSTRATE GETTING VALUES FROM THE HASH TABLE
 let p = tree.getValue(jlKey);
 console.log("\nget " + jlKey + ": " + p.toString() + "\n");
 p = tree.getValue(cwKey);
@@ -47,17 +47,17 @@ p = tree.getValue(dgKey);
 console.log("\nget " + dgKey + ": " + p.toString() + "\n");
 
 // NOW LET'S TRY REPLACING THE DATA IN THE ABOVE THREE
-tree.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
-tree.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
-tree.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
+tree.putValue(jlKey, new Student(jlKey, "Captian " , "Marvel", 3.5));
+tree.putValue(cwKey, new Student(cwKey, "Wanda", "Maximoff", 3.89));
+tree.putValue(dgKey, new Student(dgKey, "War", "Machine", 3.4));
 printBST("\nAfter Changing 3 Items", tree);
 
 // AND DEMONSTRATE REMOVING ITEMS FROM THE BST
 tree.removeValue(jlKey);
-printBST("\nAfter Removing Otis Redding", tree);
+printBST("\nAfter Removing Captian Marvel", tree);
 
 tree.removeValue(cwKey);
-printBST("\nAfter Removing Keith Richards", tree);
+printBST("\nAfter Removing Wanda Maximoff", tree);
 
 tree.removeValue(dgKey);
-printBST("\nAfter Removing Bill Withers", tree);
+printBST("\nAfter Removing War Machine", tree);
